cc8da51f69730729b2229ddd64ccf696f5753544
ArunachalamJegadeesan <inboxofarun@gmail.com>
2019-10-18 09:01:20 +0530

    first commit

---
+++ src/main/java/com/springboot/ng/SpringBootNGApp.java
@@ -0,0 +1,12 @@
+package com.springboot.ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCRUDApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCRUDApp.class, args);
+	}
+}
